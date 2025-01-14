90b6046a9be6513ee1d4c96c035c2775885598b3
shubhanshu-02 <shubhanshu004@gmail.com>
2025-01-14 20:24:54 +0530

    newer build.gradle

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+import 'package:intelligent_zoom/detection.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final cameras = await availableCameras();
+  runApp(MyApp(cameras: cameras));
+}
+
+class MyApp extends StatelessWidget {
+  final List<CameraDescription> cameras;
+
+  const MyApp({super.key, required this.cameras});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Detection(
+        cameras: cameras,
+      ),
+    );
+  }
+}
